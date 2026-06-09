79ff341d0e1a972003f1c498642812233ddede3f
Elton Ribeiro <contato.eltonribeiro@gmail.com>
2026-06-09 11:01:01 -0300

    the skeleton

---
+++ rbnfuzzer.py
@@ -0,0 +1,14 @@
+import argparse
+
+import requests
+
+
+parser: argparse.ArgumentParser = argparse.ArgumentParser(prog='Fuzzer',
+                                                          description='Fuzzing for directories list',
+                                                          epilog='Help you')
+
+parser.add_argument('-u', '--url')
+parser.add_argument('-w', '--wordlist')
+
+args: argparse.Namespace = parser.parse_args()  #  <- Lê o sys.argv (O que foi digitado pelo usuário) e devolve um Namespace com os valores
+
