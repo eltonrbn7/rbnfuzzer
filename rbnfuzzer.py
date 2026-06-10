8b8e1dd2871981862bed9db1800a7823b8ba8d69
Elton Ribeiro <contato.eltonribeiro@gmail.com>
2026-06-10 11:03:07 -0300

    feat: new argument output, with json format to 200 responses

--- rbnfuzzer.py
+++ rbnfuzzer.py
@@ -1,15 +1,17 @@
 import argparse
 import requests
 import sys
+import json
 
 def get_args() -> argparse.Namespace:
-    parser: argparse.ArgumentParser = argparse.ArgumentParser(prog='Fuzzer',
-                                                            description='Fuzzing for directories list',
-                                                            epilog='Help you',
-                                                            usage='Example: python rbnfuzzer.py -u https://target.com -w path/wordlist')
+    parser = argparse.ArgumentParser(prog='Fuzzer',
+                                    description='Fuzzing for directories list',
+                                    epilog='Help you',
+                                    usage='Example: python rbnfuzzer.py -u https://target.com -w path/wordlist')
 
     parser.add_argument('-u', '--url', required=True)
     parser.add_argument('-w', '--wordlist', required=True)
+    parser.add_argument('-o', '--output', required=False)
     parser.add_argument('-tc', '--timeconnect', required=False, type=float, default=3.0)
     parser.add_argument('-tr', '--timeread', required=False, type=float, default=5.0)
 
@@ -38,16 +40,26 @@ def read_wordlist(path: str) -> list[str]:
     return words
 
 
-def probe(url: str, wdl: list[str], timeout: tuple) -> None:
+def probe(url: str, wdl: list[str], timeout: tuple) -> list[dict]:
 
     url = url.rstrip('/')
     paths = [url + "/" + word for word in wdl]
 
+    results = [] 
+
     for path in paths:
+
         try:
             req = requests.get(path, timeout=timeout)
             status_code = req.status_code
-            print(f'Route: {path}  -  Status Code: {status_code}')
+            print(f'{path}  |  [ {status_code} ]')
+            if status_code == 200:
+                results.append(
+                    {
+                        'path': path,
+                        'status_code': status_code
+                    }
+                )
 
         except requests.exceptions.MissingSchema:
             print(f'Invalid URL {path}: No scheme supplied. Perhaps you meant https://{path}')
@@ -56,8 +68,17 @@ def probe(url: str, wdl: list[str], timeout: tuple) -> None:
         except requests.exceptions.ReadTimeout:
             print(f'Read Timeout Error: {path}')
 
+    return results
+
+
+def save_output(results: list[dict], output: str) -> None:
+    if output:
+        with open(output+".json", 'w') as o_file:
+                json.dump(results, o_file)
+
 
 if __name__ == "__main__":
     args = get_args()
     words = read_wordlist(path=args.wordlist)
-    probe(url=args.url, wdl=words, timeout=(args.timeconnect, args.timeread))
+    results = probe(url=args.url, wdl=words, timeout=(args.timeconnect, args.timeread))
+    save_output(results, args.output)
